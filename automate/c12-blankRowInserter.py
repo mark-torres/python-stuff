397dfb0324b8b69243ba707999379d3d48a69d90
Mark Torres <mark.torres.mets@gmail.com>
2015-12-31 16:35:12 -0600

    Chapter 12 (ii)

--- automate/c12-blankRowInserter.py
+++ automate/c12-blankRowInserter.py
@@ -1,1 +1,72 @@
 #!/usr/bin/env python3
+import argparse, openpyxl, sys, os
+from openpyxl.cell import get_column_letter
+
+parser = argparse.ArgumentParser(description='Inserts N blank rows at desired position in a spreadsheet')
+
+parser.add_argument('--number','-n',
+	dest='nRows',
+	type=int,
+	required=True,
+	nargs=1,
+	help="Rows to insert. Integer in the range 1 - 100")
+parser.add_argument('--row','-r',
+	dest='startRow',
+	type=int,
+	required=True,
+	nargs=1,
+	help="Row number where the rows will be inserted. Positive integer in the range from 1 to the number of rows in the spreadsheet.")
+parser.add_argument('--file','-f',
+	dest='targetFile',
+	required=True,
+	nargs=1,
+	help="Target XLSX file")
+args = parser.parse_args()
+
+# required args
+nRows = args.nRows[0]
+startRow = args.startRow[0]
+targetFile = args.targetFile[0]
+
+# VALIDATE ARGUMENTS
+# nRows
+if nRows < 1 or nRows > 100:
+	print("Number of rows to insert must be in the range 1 - 100")
+	sys.exit()
+
+# targetFile
+if not os.path.exists(targetFile):
+	print("Target file does not exist")
+	sys.exit()
+try:
+	wb = openpyxl.load_workbook(targetFile)
+except:
+	print( "Error reading file %s" % (targetFile) )
+	sys.exit()
+
+# get highest row and column
+sheet = wb.active
+# maxCol = sheet.get_highest_column()
+maxCol = sheet.max_column
+# maxRow = sheet.get_highest_row()
+maxRow = sheet.max_row
+
+if maxRow < startRow:
+	print("Start row must be less than the maximum number of rows in the spreadsheet")
+	sys.exit()
+
+# move data
+lastRow = maxRow + nRows
+for row in range(lastRow, lastRow - nRows - 1, -1):
+	for col in range(1, maxCol + 1, 1):
+		dstCell = "%s%d" % (get_column_letter(col), row)
+		srcCell = "%s%d" % (get_column_letter(col), row - nRows)
+		sheet[dstCell] = sheet[srcCell].value
+
+# erase inserted rows
+for row in range(startRow, startRow + nRows, 1):
+	for col in range(1, maxCol + 1, 1):
+		cell = "%s%d" % (get_column_letter(col), row)
+		sheet[cell] = ''
+
+wb.save(targetFile)
--- automate/c12-multiplicationTable.py
+++ automate/c12-multiplicationTable.py
@@ -1,1 +1,66 @@
 #!/usr/bin/env python3
+import argparse, openpyxl, sys
+from openpyxl.styles import Font, Style
+from openpyxl.cell import get_column_letter
+
+parser = argparse.ArgumentParser(description='Makes a spreadsheet with multiplication table in it')
+
+parser.add_argument('--number','-n',
+	dest='number',
+	type=int,
+	required=True,
+	nargs=1,
+	help="Maximum number for the multiplication table.")
+parser.add_argument('--output','-o',
+	dest='outFile',
+	required=True,
+	nargs=1,
+	help="Output XLSX file")
+args = parser.parse_args()
+
+# required args
+number = args.number[0]
+outFile = args.outFile[0]
+
+if number < 1 or number > 100:
+	print('Number out of range. Please enter an integer between 1 and 100')
+	sys.exit()
+
+# create style object
+boldFont = Font(bold=True)
+styleBold = Style(font = boldFont)
+
+# create woorkbook
+wb = openpyxl.Workbook()
+sheet = wb.active
+
+# iterate values
+maxNum = number + 1
+for row in range(1, maxNum + 1):
+	for col in range(1, maxNum + 1):
+		# calculate cell coordinate
+		colLetter = get_column_letter(col)
+		cell = "%s%d" % (colLetter, row)
+		# calculate cell value
+		if row == 1 and col == 1:
+			result = " "
+		elif row == 1 and col > 1:
+			result = "%d" % (col - 1)
+			sheet[cell].style = styleBold
+		elif row > 1 and col == 1:
+			result = "%d" % (row - 1)
+			sheet[cell].style = styleBold
+		else:
+			result = "%d" % ((col - 1) * (row - 1))
+		sheet[cell] = result
+
+# freeze pane
+sheet.freeze_panes = 'B2'
+
+if not outFile.endswith(".xlsx"):
+	outFile = outFile + ".xlsx"
+
+try:
+	wb.save(outFile)
+except:
+	print("Error saving file.")
