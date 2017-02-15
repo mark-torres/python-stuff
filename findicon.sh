b0d6a9f9c159cb8b8c1c4aa908e179cc348f1565
Mark Torres <mark.torres.mets@gmail.com>
2017-02-14 18:49:20 -0600

    Improvements

--- findicon.sh
+++ findicon.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ICONS_DIR=/Users/markushi/vhosts/material-icons.local/htdocs
-find $ICONS_DIR/*/drawable*dpi -type f -name "ic_*$1*"
+find $ICONS_DIR/*/drawable-xxxhdpi -type f -name "ic_*$1*"
